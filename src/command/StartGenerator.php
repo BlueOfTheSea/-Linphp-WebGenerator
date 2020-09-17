f0259369a7111328aef221ce1851475ab391f093
houlinlin <houlinlin@kaituocn.com>
2020-09-17 13:40:04 +0800

    为骨架追加功能

--- src/command/StartGenerator.php
+++ src/command/StartGenerator.php
@@ -43,6 +43,7 @@ class StartGenerator extends Command
     public function start($name)
     {
         if (!$name) {
+            $this->deldir(app_path().'model\\entity');
             (new EntityModelGenerator())->command();
             return;
         }
@@ -87,5 +88,32 @@ class StartGenerator extends Command
         }
     }
 
+    /**
+     * 删除model/entity实体模型
+     * @author Administrator
+     * @param $dir
+     * @return bool
+     */
+    public function deldir($dir) {
+        //先删除目录下的文件：
+        $dh=opendir($dir);
+        while ($file=readdir($dh)) {
+            if($file!="." && $file!="..") {
+                $fullpath=$dir."/".$file;
+                if(!is_dir($fullpath)) {
+                    unlink($fullpath);
+                } else {
+                    deldir($fullpath);
+                }
+            }
+        }
 
+        closedir($dh);
+        //删除当前文件夹：
+        if(rmdir($dir)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
